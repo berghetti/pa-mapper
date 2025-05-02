0891b242accffc9e16e51abd7e111f5618b35cdd
berghetti <40703575+berghetti@users.noreply.github.com>
2025-05-02 12:20:34 -0400

    Update pa-mapper.py

--- pa-mapper.py
+++ pa-mapper.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 
 import sys
@@ -11,95 +12,112 @@ from mods.pa import Pa
 
 CONFIG_FILE = None
 update_interval = 15
+omada_enable = False
+unifi_enable = False
+
+unifi = None
+omada = None
 
 def valid_user(user):
     user = user.strip()
-
-    # Unifi has NULL char end string, remove this
-    user = user.rstrip('\x00')
-
-    if user == 'anonymous' or user == '': return False
-
+    user = user.rstrip('\x00')  # Remove null char
+    if user == 'anonymous' or user == '':
+        return False
     return user
 
 def get_unifi_users():
-    unifi = Unifi(config=CONFIG_FILE)
-    unifi.login()
-
     users = []
     for client in unifi.getClients():
-        if client['is_wired'] == True or \
-                '1x_identity' not in client or 'ip' not in client:
+        if client['is_wired'] or '1x_identity' not in client or 'ip' not in client:
             continue
 
-        user = client['1x_identity']
-        user = valid_user(user)
-        if user == False: continue
+        user = valid_user(client['1x_identity'])
+        if not user:
+            continue
 
         ip = client['ip'].strip()
-        if ip == '': continue
+        if ip == '':
+            continue
 
         users.append({'user': user, 'ip': ip})
-
-    unifi.logout()
 
     return users
 
 def get_omada_users():
-    omada = Omada(config=CONFIG_FILE)
-    omada.login()
-
     users = []
     for client in omada.getSiteClients():
-        if client['wireless'] == False or \
-                'ip' not in client or 'dot1xIdentity' not in client:
+        if not client['wireless'] or 'ip' not in client or 'dot1xIdentity' not in client:
             continue
 
-        user = client['dot1xIdentity']
-        user = valid_user(user)
-        if user == False: continue
+        user = valid_user(client['dot1xIdentity'])
+        if not user:
+            continue
 
         ip = client['ip'].strip()
-        if ip == '': continue
+        if ip == '':
+            continue
 
         users.append({'user': user, 'ip': ip})
-
-    omada.logout()
 
     return users
 
 def do_user_mapping(pa):
     while True:
         users = []
-        users += get_omada_users()
-        users += get_unifi_users()
+        if omada_enable:
+            users += get_omada_users()
+        if unifi_enable:
+            users += get_unifi_users()
 
         for user in users:
             pa.add_entry(user['user'], user['ip'])
 
-        # send mapps to pa
-        if len(users) > 0:
-            if pa.mapp() == False:
+        if users:
+            if not pa.mapp():
                 logging.error('Error map')
-
             logging.info(f'Updated {len(users)} users')
 
         time.sleep(update_interval)
 
 def setup():
-    global CONFIG_FILE, update_interval
+    global CONFIG_FILE, update_interval, omada_enable, unifi_enable, unifi, omada
     CONFIG_FILE = sys.argv[1]
 
     config = ConfigParser()
     config.read(CONFIG_FILE)
-    update_interval = int(config['common']['update'])
+
+    update_interval = int(config['common'].get('update', 15))
     LOG_CODES = {'DEBUG': 10, 'INFO': 20, 'WARNING': 30, 'ERROR': 40}
-    lvl = config['common']['log']
-    debug = LOG_CODES[lvl]
+    lvl = config['common'].get('log', 'INFO')
+    debug = LOG_CODES.get(lvl.upper(), 20)
+
+    omada_enable = config.getboolean('omada', 'enable', fallback=False)
+    unifi_enable = config.getboolean('unifi', 'enable', fallback=False)
 
     logging.basicConfig(filename='/var/log/pa_mapper.log', level=debug,
                         format='%(asctime)s - %(levelname)s - %(message)s',
                         datefmt='%d-%m-%Y %H:%M:%S')
+
+    if omada_enable:
+        omada = Omada(config=CONFIG_FILE)
+        if not omada.login():
+            logging.error('Omada login failed')
+            omada_enable = False
+        else:
+            logging.info('Omada login successful')
+    else:
+            logging.info('Omada disabled')
+
+    if unifi_enable:
+        unifi = Unifi(config=CONFIG_FILE)
+        if not unifi.login():
+            logging.error('Unifi login failed')
+            unifi_enable = False
+        else:
+            logging.info('Unifi login successful')
+    else:
+            logging.info('Unifi disabled')
+
 
 def main():
     setup()
@@ -107,12 +125,18 @@ def main():
     logging.info('Starting pa-mapper')
 
     pa = Pa(config=CONFIG_FILE)
-    if pa.login() == False:
+    if not pa.login():
         logging.error('Error PA Login')
         exit(1)
 
-    do_user_mapping(pa)
-    logging.info('Stoping pa-mapper')
+    try:
+        do_user_mapping(pa)
+    finally:
+        if omada_enable and omada:
+            omada.logout()
+        if unifi_enable and unifi:
+            unifi.logout()
+        logging.info('Stopping pa-mapper')
 
 if __name__ == '__main__':
     main()
